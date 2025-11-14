b9d8f27e5eaa625fbe01221b770e87bf0a23275b
shayeliza <shaylee.neveling@lis.ac.uk>
2025-11-14 12:37:15 +0000

    "Pick a module" with dropdown containing all unique modules in a page_fluid UI added only

--- app.py
+++ app.py
@@ -3,21 +3,35 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from shiny import App, render, ui, reactive
-from shinywidgets import output_widget, render_widget
+
+# this is my clean dataframe exported as a CSV file so that I don't have to clean it again. 
+df = pd.read_csv('shiny_dataframe.csv')
 
 
+module_choices = df['Module Name'].unique().tolist()
 
 # UI
-ui.page_fluid(
+app_ui = ui.page_fluid(
     ui.input_select( "Module", "Pick a module",
-                    "choices" = [], selected = None), 
+                    choices =module_choices, selected = None), 
 )
 
 
 # Server 
+def server (input, output, session):
+    @reactive.calc # will return a value and will only recalculate when dependencies change
+    def filtered_data():
+        selected_module = input.module()
+        if selected_module:
+            module_df = df[df['Module Name'] == selected_module].copy()
+# code from my notebook which creates a filtered dataframe dependant on which module is chosen (selected_module) from module_choices
+            date_grouped = module_df.groupby('Date')
+            attendance_series = date_grouped['Attended'].mean()
+            attendance_df = attendance_series.to_frame(name=f'avg_attendance for {selected_module} module')
 
-
-
+            return attendance_df
+        return pd.DataFrame()
+    
 
 # App 
 app = App(app_ui, server)
