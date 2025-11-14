437eb3bc8d46fd6097463049007bf452ab3f0957
shayeliza <shaylee.neveling@lis.ac.uk>
2025-11-14 13:42:28 +0000

    working graph app with L6 addition - the ability to pick a module. Mean line included

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ module_choices = df['Module Name'].unique().tolist()
 
 # UI
 app_ui = ui.page_fluid(
-    ui.input_select( "Module", "Pick a module",
-                    choices =module_choices, selected = None), 
-)
+    ui.input_select( "module", "Pick a module",
+                    choices =module_choices, selected = None
+    ),
 
+    ui.output_plot("attendance_plot", height='600px')
+)
 
 # Server 
 def server (input, output, session):
@@ -27,11 +29,29 @@ def server (input, output, session):
 # code from my notebook which creates a filtered dataframe dependant on which module is chosen (selected_module) from module_choices
             date_grouped = module_df.groupby('Date')
             attendance_series = date_grouped['Attended'].mean()
-            attendance_df = attendance_series.to_frame(name=f'avg_attendance for {selected_module} module')
+            attendance_df = attendance_series.to_frame(name = 'avg_attendance')
 
             return attendance_df
         return pd.DataFrame()
     
+    @output
+    @render.plot
+    def attendance_plot():
+        attendance = filtered_data()
+        
+        if attendance.empty: 
+            return plt.figure() 
+        else:
+            fig, ax = plt.subplots(figsize=(15,6))
+            attendance['avg_attendance'].plot(kind='bar', ax=ax)
+            plt.axhline(y=attendance['avg_attendance'].mean(), color='red', linestyle='--', label=f'Mean: {attendance["avg_attendance"].mean():.3f}')
+            plt.xlabel('Date')
+            plt.ylabel('Average attendance')
+            plt.xticks(rotation=45)
+            plt.legend()
+            plt.tight_layout()  # doing this to prevent labels from getting cut iff
+            
+        return fig 
 
 # App 
 app = App(app_ui, server)
